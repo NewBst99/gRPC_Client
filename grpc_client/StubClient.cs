f7cc8b0016331ead4924d72c6af423aa342c0121
mbjin <mbjin118@naver.com>
2024-10-08 15:07:15 +0900

    1008(Grpc.Net.Client)

--- grpc_client/StubClient.cs
+++ grpc_client/StubClient.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Grpc.Core;
+using Grpc.Net.Client;
 using myungbae;  // gRPC 생성된 파일들 포함
 
 namespace grpc_client
 {
     public class StubClient
     {
-        private readonly Channel channel;
+        private readonly GrpcChannel channel;
         private readonly GameService.GameServiceClient client;
 
         public StubClient(string host, int port)
         {
-            channel = new Channel(host, port, ChannelCredentials.Insecure);
+            channel = GrpcChannel.ForAddress("http://localhost:9090");
             client = new GameService.GameServiceClient(channel);
         }
 
@@ -104,11 +104,11 @@ namespace grpc_client
             var client = new StubClient("localhost", 9090);
 
             // Save User Info
-            client.saveUserInfo(1, "myungbae", 80, "1999-07-16 21:57:15");
-            client.saveUserInfo(2, "seungtae", 40, "1945-07-16 19:45:35");
-            client.saveUserInfo(3, "daewook", 58, "2000-12-07 15:19:41");
-            client.saveUserInfo(4, "junyeong", 21, "1873-07-16 11:23:14");
-            client.saveUserInfo(5, "minguk", 52, "2000-03-07 15:35:24");
+            client.saveUserInfo(1, "user1", 80, "2024-10-08 21:57:15");
+            client.saveUserInfo(2, "user2", 40, "2024-10-03 19:45:35");
+            client.saveUserInfo(3, "user3", 58, "2024-06-07 15:19:41");
+            client.saveUserInfo(4, "user4", 21, "2024-07-10 11:23:14");
+            client.saveUserInfo(5, "user5", 52, "2024-03-07 15:35:24");
 
             // Save Map Progress
             client.saveMapProgress(1, 0, -40, -8, 0);
@@ -117,16 +117,16 @@ namespace grpc_client
             // Save Item Relations
             client.saveItemRelation(1, "c100", 1);
             client.saveItemRelation(1, "c101", 2);
-            client.saveItemRelation(3, "c100", 1);
-            client.saveItemRelation(3, "c101", 1);
+            client.saveItemRelation(2, "c100", 1);
+            client.saveItemRelation(2, "c101", 1);
 
             client.getUserInfo(1);
             client.getMapProgress(1);
             client.getItemRelation(1);
 
-            client.getUserInfo(3);
-            client.getMapProgress(3);
-            client.getItemRelation(3);
+            client.getUserInfo(2);
+            client.getMapProgress(2);
+            client.getItemRelation(2);
 
             client.ShutdownAsync().Wait();
         }
