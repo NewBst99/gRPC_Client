ea521771841b30832b1d3a63d5f1b174de4c0923
mbjin <90257022+NewBst99@users.noreply.github.com>
2024-10-28 19:25:48 +0900

    Grpc.Net.Client -> Grpc.Core

--- StubClient.cs
+++ StubClient.cs
@@ -1,134 +1,98 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
-using Grpc.Net.Client;
+using Grpc.Core;
 using myungbae;  // gRPC 생성된 파일들 포함
 
-namespace grpc_client
+public class StubClient
 {
-    public class StubClient
+    private readonly Channel channel;
+    private readonly GameService.GameServiceClient client;
+
+    public StubClient(string host, int port)
     {
-        private readonly GrpcChannel channel;
-        private readonly GameService.GameServiceClient client;
+        // Grpc.Core의 Channel을 사용하여 GrpcChannel을 대체
+        channel = new Channel(host, port, ChannelCredentials.Insecure); // HTTP/2 보안을 비활성화 (개발 환경에서만 사용 권장)
+        client = new GameService.GameServiceClient(channel);
+    }
 
-        public StubClient(string host, int port)
+    public void getUserInfo(int userid)
+    {
+        var request = new UserInfoRequest { Userid = userid };
+        var response = client.getUserInfo(request);
+
+        Console.WriteLine(response.Checkmessage);
+        Console.WriteLine("User ID : " + response.Userid);
+        Console.WriteLine("Nickname : " + response.Nkname);
+        Console.WriteLine("Experience : " + response.Exp);
+        Console.WriteLine("Save Time : " + response.Savetime);
+    }
+
+    public void saveUserInfo(int userid, string nkname, int exp, string savetime)
+    {
+        var request = new UserInfoRequest
         {
-            channel = GrpcChannel.ForAddress($"http://{host}:{port}");
-            client = new GameService.GameServiceClient(channel);
-        }
+            Userid = userid,
+            Nkname = nkname,
+            Exp = exp,
+            Savetime = savetime
+        };
 
-        public void getUserInfo(int userid)
+        var response = client.saveUserInfo(request);
+        Console.WriteLine(response.Checkmessage);
+    }
+
+    public void getMapProgress(int userid)
+    {
+        var request = new MapProgressRequest { Userid = userid };
+        var response = client.getMapProgress(request);
+
+        Console.WriteLine(response.Checkmessage);
+        Console.WriteLine("User ID : " + response.Userid);
+        Console.WriteLine("User Location : " + response.Xloc + ", " + response.Yloc + ", " + response.Zloc);
+        Console.WriteLine("Map Progress : " + response.Mapprogress);
+    }
+
+    public void saveMapProgress(int userid, int mapprogress, double xloc, double yloc, double zloc)
+    {
+        var request = new MapProgressRequest
         {
-            var request = new UserInfoRequest { Userid = userid };
-            var response = client.getUserInfo(request);
+            Userid = userid,
+            Mapprogress = mapprogress,
+            Xloc = xloc,
+            Yloc = yloc,
+            Zloc = zloc
+        };
 
-            Console.WriteLine(response.Checkmessage);
-            Console.WriteLine("User ID : " + response.Userid);
-            Console.WriteLine("Nickname : " + response.Nkname);
-            Console.WriteLine("Experience : " + response.Exp);
-            Console.WriteLine("Save Time : " + response.Savetime);
-        }
+        var response = client.saveMapProgress(request);
+    }
 
-        public void saveUserInfo(int userid, string nkname, int exp, string savetime)
+    public void getItemRelation(int userid)
+    {
+        var request = new ItemRelationRequest { Userid = userid };
+        var response = client.getItemRelation(request);
+
+        Console.WriteLine(response.Checkmessage);
+        Console.WriteLine("User ID : " + response.Userid);
+        Console.WriteLine("Item ID : " + response.Itemid);
+        Console.WriteLine("Item Name : " + response.Itemname);
+        Console.WriteLine("Quantity : " + response.Quantity);
+    }
+
+    public void saveItemRelation(int userid, string itemid, int quantity)
+    {
+        var request = new ItemRelationRequest
         {
-            var request = new UserInfoRequest
-            {
-                Userid = userid,
-                Nkname = nkname,
-                Exp = exp,
-                Savetime = savetime
-            };
+            Userid = userid,
+            Itemid = itemid,
+            Quantity = quantity
+        };
 
-            var response = client.saveUserInfo(request);
-            Console.WriteLine(response.Checkmessage);
-        }
+        var response = client.saveItemRelation(request);
+    }
 
-        public void getMapProgress(int userid)
-        {
-            var request = new MapProgressRequest { Userid = userid };
-            var response = client.getMapProgress(request);
-
-            Console.WriteLine(response.Checkmessage);
-            Console.WriteLine("User ID : " + response.Userid);
-            Console.WriteLine("User Location : " + response.Xloc + ", " + response.Yloc + ", " + response.Zloc);
-            Console.WriteLine("Map Progress : " + response.Mapprogress);
-        }
-
-        public void saveMapProgress(int userid, int mapprogress, double xloc, double yloc, double zloc)
-        {
-            var request = new MapProgressRequest
-            {
-                Userid = userid,
-                Mapprogress = mapprogress,
-                Xloc = xloc,
-                Yloc = yloc,
-                Zloc = zloc
-            };
-
-            var response = client.saveMapProgress(request);
-        }
-
-        public void getItemRelation(int userid)
-        {
-            var request = new ItemRelationRequest { Userid = userid };
-            var response = client.getItemRelation(request);
-
-            Console.WriteLine(response.Checkmessage);
-            Console.WriteLine("User ID : " + response.Userid);
-            Console.WriteLine("Item ID : " + response.Itemid);
-            Console.WriteLine("Item Name : " + response.Itemname);
-            Console.WriteLine("Quantity : " + response.Quantity);
-        }
-
-        public void saveItemRelation(int userid, string itemid, int quantity)
-        {
-            var request = new ItemRelationRequest
-            {
-                Userid = userid,
-                Itemid = itemid,
-                Quantity = quantity
-            };
-
-            var response = client.saveItemRelation(request);
-        }
-
-        public async Task ShutdownAsync()
-        {
-            await channel.ShutdownAsync();
-        }
-
-        // Functional separation
-        // public static void Main(string[] args)
-        // {
-        //     var client = new StubClient("localhost", 9090);
-
-        //     // Save User Info
-        //     client.saveUserInfo(1, "user1", 80, "2024-10-08 21:57:15");
-        //     client.saveUserInfo(2, "user2", 40, "2024-10-03 19:45:35");
-        //     client.saveUserInfo(3, "user3", 58, "2024-06-07 15:19:41");
-        //     client.saveUserInfo(4, "user4", 21, "2024-07-10 11:23:14");
-
-        //     // Save Map Progress
-        //     client.saveMapProgress(1, 0, -40, -8, 0);
-        //     client.saveMapProgress(2, 1, -160, -52, 9);
-
-        //     // Save Item Relations
-        //     client.saveItemRelation(1, "c100", 1);
-        //     client.saveItemRelation(1, "c101", 2);
-        //     client.saveItemRelation(2, "c100", 1);
-        //     client.saveItemRelation(2, "c101", 1);
-
-        //     client.getUserInfo(1);
-        //     client.getMapProgress(1);
-        //     client.getItemRelation(1);
-
-        //     client.getUserInfo(2);
-        //     client.getMapProgress(2);
-        //     client.getItemRelation(2);
-
-            // client.ShutdownAsync().Wait();
-        // }
+    public async Task ShutdownAsync()
+    {
+        await channel.ShutdownAsync();
     }
 }
